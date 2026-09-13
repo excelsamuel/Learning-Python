87d3b1fa4b8147444244d0f84d39b887fc551054
Excel Samuel <103432683+excelsamuel@users.noreply.github.com>
2026-09-13 14:56:09 +0100

    Asked if you went to church

---
+++ Church trying.py
@@ -0,0 +1,7 @@
+church = input("Did you attend chuch today? ")
+if church.lower() == "yes":
+    print ("Awsome, I am glad you went to church today")
+elif church.lower() == "no":
+    print ("Okay, Ensure you dont miss next week's own")
+else:
+    print ("Your answer should be only either (yes/no)")
