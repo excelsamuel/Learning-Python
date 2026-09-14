5f2fed197a4ec8c74bef7e16464891bed7f05698
Excel Samuel <103432683+excelsamuel@users.noreply.github.com>
2026-09-14 20:34:26 +0100

    just random

---
+++ random working.py
@@ -0,0 +1,16 @@
+
+# Trying to know how the .lower works lol
+church = input("Did you attend chuch today? ")
+if church.lower() == "yes":
+    print ("Awsome, I am glad you went to church today")
+elif church.lower() == "no":
+    print ("Okay, Ensure you dont miss next week's own")
+else:
+    print ("Your answer should be only either (yes/no)")
+
+
+    # Tyring someting out
+    # Trying to know how to fix a call btw strings
+age = float(input("Enter your age: "))
+print ("I am %d years old" %(age))
+print (f"I am {age} years old")
